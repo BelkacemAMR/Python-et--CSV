7a5f105370fe1e5ccd24fdb24484d461f7863951
Belkacem <abel-and-co@hotmail.com>
2023-03-31 21:33:02 +0200

    Ajout fonction generate_csv(data)

--- main.py
+++ main.py
@@ -1,2 +1,25 @@
 import csv
 import datetime
+
+
+###########################################################################################
+def generate_csv(data):
+    keys = [key for key, value in data[0]]
+
+    with open('results.csv', 'w', newline='') as csvfile:
+        writer = csv.DictWriter(csvfile, fieldnames=keys, delimiter=',', quotechar='"', quoting=csv.QUOTE_MINIMAL)
+        writer.writeheader()
+
+        for row in data:
+            row_data = {}
+            for key, value in row:
+
+                if key == 'date':
+                    value = value.strftime('%m/%d/%Y')
+
+
+                elif isinstance(value, (list, tuple)):
+                    value = ",".join(value)
+                row_data[key] = value
+            writer.writerow(row_data)
+
